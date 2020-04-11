d78dfc0669a2aa3c41ce2ee0e36a58ffc9d94347
Andrey Ermilov <andrerm@ya.ru>
2020-04-11 19:20:09 +0300

    issue-5: Add test json pyalod

--- tests/test_json_validation.rs
+++ tests/test_json_validation.rs
@@ -6,14 +6,14 @@ use serde_derive::{Deserialize, Serialize};
 use actix_web_validator::ValidatedJson;
 
 #[derive(Debug, Validate, Serialize, Deserialize)]
-struct JsonData {
+struct JsonPayload {
    #[validate(url)]
    page_url: String,
    #[validate(range(min = 18, max = 28))]
    age: u8,
 }
 
-fn test_handler(_query: ValidatedJson<JsonData>) -> HttpResponse {
+fn test_handler(_query: ValidatedJson<JsonPayload>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
@@ -27,7 +27,7 @@ fn test_json_validation() {
     // Test 200 status
     let req = test::TestRequest::post()
         .uri("/test")
-        .set_json(&JsonData { page_url: "https://my_page.com".to_owned(), age: 24 })
+        .set_json(&JsonPayload { page_url: "https://my_page.com".to_owned(), age: 24 })
         .to_request();
     let resp = test::block_on(app.call(req)).unwrap();
     assert_eq!(resp.status(), StatusCode::OK);
@@ -35,7 +35,31 @@ fn test_json_validation() {
     // Test 400 status
     let req = test::TestRequest::post()
         .uri("/test")
-        .set_json(&JsonData { page_url: "invalid_url".to_owned(), age: 24 })
+        .set_json(&JsonPayload { page_url: "invalid_url".to_owned(), age: 24 })
+        .to_request();
+    let resp = test::block_on(app.call(req)).unwrap();
+    assert_eq!(resp.status(), StatusCode::BAD_REQUEST);
+}
+
+#[test]
+fn test_custom_validation_error() {
+        let mut app = test::init_service(
+        App::new()
+            .service(web::resource("/test").route(web::post().to(test_handler)))
+    );
+
+    // Test 200 status
+    let req = test::TestRequest::post()
+        .uri("/test")
+        .set_json(&JsonPayload { page_url: "https://my_page.com".to_owned(), age: 24 })
+        .to_request();
+    let resp = test::block_on(app.call(req)).unwrap();
+    assert_eq!(resp.status(), StatusCode::OK);
+
+    // Test 400 status
+    let req = test::TestRequest::post()
+        .uri("/test")
+        .set_json(&JsonPayload { page_url: "invalid_url".to_owned(), age: 24 })
         .to_request();
     let resp = test::block_on(app.call(req)).unwrap();
     assert_eq!(resp.status(), StatusCode::BAD_REQUEST);
