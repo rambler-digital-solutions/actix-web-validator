d5cb7a0227c3e8425275337aad4a5395017aa8d4
Andrey Ermilov <andrerm@ya.ru>
2020-04-11 19:56:30 +0300

    issue-5: Add custom error handling test

--- tests/test_json_validation.rs
+++ tests/test_json_validation.rs
@@ -1,5 +1,5 @@
 use actix_service::Service;
-use actix_web::{test, web, HttpResponse, http::StatusCode, App};
+use actix_web::{test, web, HttpResponse, http::StatusCode, App, error};
 use validator::Validate;
 use validator_derive::Validate;
 use serde_derive::{Deserialize, Serialize};
@@ -43,24 +43,20 @@ fn test_json_validation() {
 
 #[test]
 fn test_custom_validation_error() {
-        let mut app = test::init_service(
+    let mut app = test::init_service(
         App::new()
-            .service(web::resource("/test").route(web::post().to(test_handler)))
+        .data(actix_web_validator::JsonConfig::default()
+            .error_handler(|err, _req| {
+                error::InternalError::from_response(
+                    err, HttpResponse::Conflict().finish()).into()
+            }))
+        .service(web::resource("/test").to(test_handler))
     );
 
-    // Test 200 status
     let req = test::TestRequest::post()
         .uri("/test")
-        .set_json(&JsonPayload { page_url: "https://my_page.com".to_owned(), age: 24 })
+        .set_json(&JsonPayload { page_url: "invalid".to_owned(), age: 24 })
         .to_request();
     let resp = test::block_on(app.call(req)).unwrap();
-    assert_eq!(resp.status(), StatusCode::OK);
-
-    // Test 400 status
-    let req = test::TestRequest::post()
-        .uri("/test")
-        .set_json(&JsonPayload { page_url: "invalid_url".to_owned(), age: 24 })
-        .to_request();
-    let resp = test::block_on(app.call(req)).unwrap();
-    assert_eq!(resp.status(), StatusCode::BAD_REQUEST);
+    assert_eq!(resp.status(), StatusCode::CONFLICT);
 }
