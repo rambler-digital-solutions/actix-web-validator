b9f275818692ca12b49ea590929228c1d839a24f
Belousov Maksim <lib.bmw@gmail.com>
2022-07-18 15:42:23 +0300

    Add `#[inline]` and indent value

--- src/error.rs
+++ src/error.rs
@@ -48,7 +48,8 @@ impl ResponseError for Error {
                     "Validation errors in fields:\n{}",
                     flatten_errors(e)
                         .iter()
-                        .map(|(field, err)| { format!("\t{}: {}", field, err) })
+                        .map(|(_, field, err)| { 
+                            format!("\t{}: {}", field, err) })
                         .collect::<Vec<_>>()
                         .join("\n")
                 )
@@ -61,18 +62,22 @@ impl ResponseError for Error {
 /// Helper function for error extraction and formatting.
 /// Return Vec of tuples where first element is full field path (separated by dot)
 /// and second is error.
-pub fn flatten_errors(errors: &ValidationErrors) -> Vec<(String, &ValidationError)> {
-    _flatten_errors(errors, None)
+#[inline]
+pub fn flatten_errors(errors: &ValidationErrors) -> Vec<(u16, String, &ValidationError)> {
+    _flatten_errors(errors, None, None)
 }
 
+#[inline]
 fn _flatten_errors(
     errors: &ValidationErrors,
     path: Option<String>,
-) -> Vec<(String, &ValidationError)> {
+    indent: Option<u16>,
+) -> Vec<(u16, String, &ValidationError)> {
     errors
         .errors()
         .iter()
         .map(|(&field, err)| {
+            let indent = indent.unwrap_or(0);
             let actual_path = path
                 .as_ref()
                 .map(|path| [path.as_str(), field].join("."))
@@ -80,18 +85,18 @@ fn _flatten_errors(
             match err {
                 ValidationErrorsKind::Field(field_errors) => field_errors
                     .iter()
-                    .map(|error| (actual_path.clone(), error))
+                    .map(|error| (indent, actual_path.clone(), error))
                     .collect::<Vec<_>>(),
                 ValidationErrorsKind::List(list_error) => list_error
                     .iter()
                     .map(|(index, errors)| {
                         let actual_path = format!("{}[{}]", actual_path.as_str(), index);
-                        _flatten_errors(errors, Some(actual_path))
+                        _flatten_errors(errors, Some(actual_path), Some(indent + 1))
                     })
                     .flatten()
                     .collect::<Vec<_>>(),
                 ValidationErrorsKind::Struct(struct_errors) => {
-                    _flatten_errors(struct_errors, Some(actual_path))
+                    _flatten_errors(struct_errors, Some(actual_path), Some(indent + 1))
                 }
             }
         })
